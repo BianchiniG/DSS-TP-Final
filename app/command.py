65798f30f9972aab541a64217fa9d0bf003069ed
Germán <bianchiniger@gmail.com>
2020-12-02 02:00:01 -0300

    Agregado script de comando para entrenar/testear los modelos

---
+++ app/command.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import sys
+from modelos.RandomForest import RandomForest
+
+
+def modo_de_uso():
+    print("""Modo de uso: 
+    python command.py <nombre_modelo> <fit>|<predict> <imagen>
+        * El parámetro imagen es necesario solo para el predict
+        * Los posibles modelos son:
+            - rf (Random Forest)
+            - svm (Máquina Vectorial)
+            - cnn (Red Neuronal)
+    """)
+
+
+def get_modelo(modelo_elegido):
+    if modelo_elegido == 'rf':
+        return RandomForest()
+    elif modelo_elegido == 'svm':
+        error("Modelo no implementado")
+    elif modelo_elegido == 'cnn':
+        error("Modelo no implementado")
+    else:
+        error("El modelo elegido ("+modelo_elegido+") no existe")
+
+
+def ejecutar_comando(modelo, opciones):
+    if opciones[2] == 'fit':
+        modelo.fit()
+    elif opciones[2] == 'predict':
+        if len(opciones) < 4:
+            modo_de_uso()
+            exit(1)
+        else:
+            modelo.predict(opciones[3])
+
+
+def args_error(argumentos):
+    if len(argumentos) == 1 or len(argumentos) < 3 or argumentos[1] == 'h':
+        modo_de_uso()
+        exit(1)
+    else:
+        return False
+
+
+def error(mensaje):
+    print(mensaje)
+    exit(1)
+
+
+if not args_error(sys.argv):
+    ejecutar_comando(get_modelo(sys.argv[1]), sys.argv)
